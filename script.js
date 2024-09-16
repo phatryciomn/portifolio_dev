43bd3eeab64e61bbd39db28ea0302ceadc10ed1e
phatryciomn <phatrycio137@gmail.com>
2024-09-15 23:33:16 -0300

    3.0

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ navLinks.forEach((link) => {
     this.classList.add("active");
   });
 });
+
+const button = document.querySelector("button")
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
+
+button.onclick = function(){
+  modal.showModal()
+}
+
+buttonClose.onclick = function(){
+  modal.close()
+}
